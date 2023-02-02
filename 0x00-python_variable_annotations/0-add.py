119868a3f4019f23358d2db4c5967ae690f61deb
hanichris <hanichris71@gmail.com>
2023-02-02 16:40:21 +0300

    Find the sum of two numbers.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Type-annotated add function."""
+
+
+def add(a: float, b: float) -> float:
+    """Find the sum of two numbers.
+
+    Args:
+        a (float): first argument.
+        b (float): second argument.
+    Return:
+        a + b (float)
+    """
+    return a + b
