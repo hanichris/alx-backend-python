e18101de6c00cedbb5c430132bf2ed4013af8299
hanichris <hanichris71@gmail.com>
2023-02-02 21:13:02 +0300

    Compute the cumulative sum of a list of numbers.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Type-annotated sum_list function to sum a list of floats."""
+from functools import reduce
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Sum a list of floats and return result.
+
+    Args:
+        input_list (list[float]): list of floats.
+    Return:
+        float: cumulative sum of input.
+    """
+    return reduce(lambda x, y: x + y, input_list)
