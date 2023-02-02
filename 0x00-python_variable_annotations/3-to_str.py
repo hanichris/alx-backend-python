c8ee36d5e210757a299c3c19cf776f56d04ad71e
hanichris <hanichris71@gmail.com>
2023-02-02 20:56:03 +0300

    Determine the string represetation of a float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Type-annotated function that computes string representation."""
+
+
+def to_str(n: float) -> str:
+    """Generate the string representation of the float argument.
+
+    Args:
+        n (float): argument to convert to string.
+    Return:
+        str: string representation of the argument.
+    """
+    return str(n)
