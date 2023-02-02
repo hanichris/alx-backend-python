b4e104cbfec287abde7adf7d488b9a5bf049ccd2
hanichris <hanichris71@gmail.com>
2023-02-02 21:55:57 +0300

    Using Union to annotate an argument that can be of 2 types.

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Type-annotated function which takes only two arguments.
+
+The two arguments are a string and an int or float. It
+returns its result as a tuple.
+"""
+
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """Return the string and square of the int/float in a tuple.
+
+    Args:
+        k (str): First element of the tuple.
+        v (int | float): Argument to be squared before being returned
+                         as the second element of the tuple.
+    Return:
+        tuple: k, v^2
+    """
+    return k, v ** 2
