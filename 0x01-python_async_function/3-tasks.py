b58cc89ece19bc05c80fd871f2f694f2b6d892ab
hanichris <hanichris71@gmail.com>
2023-02-07 02:33:47 +0300

    Wrap a coroutine in a task object.

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""""""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Wrap a coroutine into a Task object and schedule its execution.
+
+    Args:
+        max_delay (int): parameter to pass to the coroutine.
+    Return:
+        asyncio.Task: Future-like object to run the coroutine.
+    """
+    return asyncio.create_task(wait_random(max_delay))
