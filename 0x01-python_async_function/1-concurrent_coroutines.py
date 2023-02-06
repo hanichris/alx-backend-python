bb8003eebbdcddb932389de4118c91a682867d1b
hanichris <hanichris71@gmail.com>
2023-02-07 01:43:45 +0300

    Function documentation.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Chain the `wait_random` coroutine.
+
+    Args:
+        n (int): number of times to call `wait_random` coroutine.
+        max_delay (int): wait time to pass to coroutine.
+    Return:
+        List[float]: list of all the `n` delay times.
+    """
     res = []
     for i in range(n):
         res.append(await wait_random(max_delay))
