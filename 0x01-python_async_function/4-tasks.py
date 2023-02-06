618fc2b887fc06d30944065c8009207016442544
hanichris <hanichris71@gmail.com>
2023-02-07 02:51:25 +0300

    fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
-"""Executing multiple coroutines at the same time with async.
-
-The asynchronous coroutine should return the list of all the delays
-in ascending order by taking advantage of concurrency and NOT the
-`sort` functionality.
-"""
+"""Contains a method that spawns Tasks n times with a
+specified delay between each call."""
 import asyncio
 from typing import List
-
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Chain the `task_wait_random` coroutine.
-
-    The returned list should be in ascending order without using
-    `sort()` functionality.
+    """Spawns wait_random n times with a specified delay
+    between each call.
     Args:
-        n (int): number of times to call `task_wait_random` coroutine.
-        max_delay (int): wait time to pass to coroutine.
-    Return:
-        List[float]: list of all the `n` delay times.
+        n: number of times to spawn wait_random
+        max_delay: maximum delay between each call
+    Returns:
+        list of delays
     """
-    coros = [task_wait_random(max_delay) for _ in range(n)]
-    return [await coro for coro in asyncio.as_completed(coros)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
