1dd145e27814c419ad8e3a000b11ac4f0af59fe6
hanichris <hanichris71@gmail.com>
2023-02-08 03:53:51 +0300

    Add `Generator` annotation.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Module defining an async generator."""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that loops 10 times waiting asynchronously.
 
     During each loop, the coroutine waits asynchronously for
