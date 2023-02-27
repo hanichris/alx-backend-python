9bf49b3b6c226671ed27badeaa20eaab4a9efe00
hanichris <hanichris71@gmail.com>
2023-02-27 20:15:38 +0300

    use @parameterized.expand decorator instead.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 """Unit test for the `utils.access_nested_map` function."""
 import unittest
-from parameterized import parameterized_class
+from parameterized import parameterized
 from utils import access_nested_map
+from typing import Any, Mapping, Sequence
 
 
-@parameterized_class(('map', 'path', 'expected'), [
-    ({'a': 1}, ('a',), 1),
-    ({'a': {'b': 2}}, ('a',), {'b': 2}),
-    ({'a': {'b': 2}}, ('a', 'b'), 2)
-])
 class TestAccessNestedMap(unittest.TestCase):
     """Test the access_nested_map funtion in the utils module."""
-    def test_access_nested_map(self):
-        """test method returns what it is supposed to."""
-        self.assertEqual(access_nested_map(self.map, self.path), self.expected)
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any) -> None:
+        """Test the method returns what it is supposed to.
 
+        Args:
+            nested_map (Mapping): A nested map.
+            path (Sequence): A sequence of keys representing the path
+                             to the value
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == "__main__":
     unittest.main()
