c635569e9e44ef1b7995d5408c3e8863512fe09d
hanichris <hanichris71@gmail.com>
2023-02-28 03:21:20 +0300

    new testcases.

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Unit test for the `client.py` module."""
+import unittest
+from client import GithubOrgClient
+from parameterized import parameterized
+from unittest.mock import patch, PropertyMock
+from utils import get_json
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test the `GithubOrgClient` class."""
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, test_org: str, mock_get) -> None:
+        """Test that GithubOrgClient.org returns the correct value.
+        
+        Args:
+            test_org (str): name of the organisation.
+            mock_get (MagicMock): created mock object.
+        """
+        client = GithubOrgClient(test_org)
+        _url = client.ORG_URL.format(org=test_org)
+        client.org()
+        mock_get.assert_called_once_with(_url)
+
+
+if __name__ == "__main__":
+    unittest.main()
